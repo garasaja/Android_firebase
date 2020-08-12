e1a8c231409d0ebcf3c7222633c001a0efb9bc5e
kdm <cokj1610@naver.com>
2020-08-12 16:37:01 +0900

    firebase 일반 user의 이메일 로그인 구현해야됨

--- app/src/main/java/com/mini/cosapp/MainActivity.java
+++ app/src/main/java/com/mini/cosapp/MainActivity.java
@@ -1,14 +1,98 @@
 package com.mini.cosapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "Main_Activity";
+    private TextView tvText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignInButton btnGoogleLogin = findViewById(R.id.btn_google_login);
+        Button btnGoogleLogout = findViewById(R.id.btn_google_logout);
+        tvText = findViewById(R.id.tv_text);
+
+        btnGoogleLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Choose authentication providers
+                List<AuthUI.IdpConfig> providers = Arrays.asList(
+                        new AuthUI.IdpConfig.GoogleBuilder().build());
+
+// Create and launch sign-in intent
+                startActivityForResult( //새로운 화면 출력
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(),
+                        3648);
+                tvText.setText("로그인 성공");
+            }
+        });
+
+        btnGoogleLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                Log.d(TAG, "onClick: 로그아웃 성공");
+                tvText.setText("로그아웃 성공");
+            }
+        });
+    }
+
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+        // [END auth_fui_signout]
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 3648) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, "onActivityResult: 로그인 완료 : " + user.getEmail());
+                Log.d(TAG, "onActivityResult: 로그인 성공시 response : " + response);
+            } else {
+                Log.d(TAG, "onActivityResult: 로그인 실패 : " );
+                Log.d(TAG, "onActivityResult: 에러내용 : " + response.getError());
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
